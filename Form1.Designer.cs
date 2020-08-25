2a78dbbb741882b6b9078030cc4299348189c79c
Alexandru Pupsa <pupsaa@live.com>
2020-08-25 17:44:46 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace PortalWSClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxFolder = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxProgres = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "txt";
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.InitialDirectory = "C:\\";
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(81, 12);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(206, 20);
+            this.textBoxInput.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Fisier intrare";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(293, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Enabled = false;
+            this.buttonStart.Location = new System.Drawing.Point(419, 12);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(110, 58);
+            this.buttonStart.TabIndex = 14;
+            this.buttonStart.Text = "Start!";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Folder iesire";
+            // 
+            // textBoxFolder
+            // 
+            this.textBoxFolder.Location = new System.Drawing.Point(81, 49);
+            this.textBoxFolder.Name = "textBoxFolder";
+            this.textBoxFolder.Size = new System.Drawing.Size(206, 20);
+            this.textBoxFolder.TabIndex = 16;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(293, 47);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Browse";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.SelectedPath = "C:\\";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 105);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Progres";
+            // 
+            // textBoxProgres
+            // 
+            this.textBoxProgres.Location = new System.Drawing.Point(81, 102);
+            this.textBoxProgres.Name = "textBoxProgres";
+            this.textBoxProgres.ReadOnly = true;
+            this.textBoxProgres.Size = new System.Drawing.Size(73, 20);
+            this.textBoxProgres.TabIndex = 19;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(580, 159);
+            this.Controls.Add(this.textBoxProgres);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBoxFolder);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxInput);
+            this.Name = "Form1";
+            this.Text = "Cautare Dosare";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxFolder;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxProgres;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,143 @@
+﻿/*
+Autor:      indaco systems (www.indaco.ro)
+Data:       21.12.2012
+Versiunea:  1.0
+Descriere:  Exemplu de apel al serviciului de acces programatic la date despre dosare http://portalquery.just.ro/query.asmx
+
+Istoric modificari:
+Data        21.12.2012
+Descriere:  Prima versiune
+ */
+using Newtonsoft.Json;
+using PortalWSClient.PortalWS;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace PortalWSClient
+{
+    public partial class Form1 : Form
+    {
+        private List<string> names = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private string FindDosareByName(string firstName, string lastName)
+        {
+            Query ws = new Query();
+            Dosar[] dosare = ws.CautareDosare(null, null, lastName, null, null, null);
+
+            List<Dosar> results = new List<Dosar>();
+
+            foreach (var dosar in dosare)
+            {
+                var found = dosar.parti.Any(x => x.nume.ToLower().Contains(lastName.ToLower()) && x.nume.ToLower().Contains(firstName.ToLower()));
+                if (found)
+                {
+                    results.Add(dosar);
+                }
+            }
+
+            if (results.Any())
+            {
+                return JsonConvert.SerializeObject(results, Formatting.Indented);
+            }
+            return null;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if (File.Exists(openFileDialog1.FileName))
+                {
+                    names = File.ReadAllLines(openFileDialog1.FileName).ToList();
+                    if (names.Any())
+                    {
+                        textBoxInput.Text = openFileDialog1.FileName;
+                        textBoxProgres.Text = $"0/{names.Count}";
+                        checkRequirements();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Fisierul nu contine niciun nume");
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Eroare deschidere fisier");
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBoxFolder.Text = folderBrowserDialog1.SelectedPath;
+                checkRequirements();
+            }
+        }
+
+        private void checkRequirements()
+        {
+            if (!string.IsNullOrEmpty(textBoxInput.Text) && !string.IsNullOrEmpty(textBoxFolder.Text) && names.Count > 0)
+            {
+                buttonStart.Enabled = true;
+            }
+            else
+            {
+                buttonStart.Enabled = false;
+            }
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            buttonStart.Enabled = false;
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+
+            BackgroundWorker worker = sender as BackgroundWorker;
+            var count = 0;
+
+            foreach (var name in names)
+            {
+                var splits = name.Split();
+                if (splits.Length > 1)
+                {
+                    var prenume = splits[0];
+                    var nume = splits[1];
+                    var rezultate = FindDosareByName(prenume, nume);
+                    if (rezultate != null)
+                    {
+                        File.WriteAllText($@"C:\temp\{prenume} {nume}.json", rezultate);
+                    }
+                }
+                count++;
+                worker.ReportProgress(count);
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            textBoxProgres.Text = $"{e.ProgressPercentage}/{names.Count}";
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            buttonStart.Enabled = true;
+            MessageBox.Show("Gata! Verifica folder-ul!");
+        }
+    }
+}
